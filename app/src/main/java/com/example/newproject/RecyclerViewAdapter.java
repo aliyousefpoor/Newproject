95a145fbbce5598edb8eb61c3a96f53986aad0b7
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2019-10-25 21:31:25 +0330

    Fix Send Data To SingleUserFragment

--- app/src/main/java/com/example/newproject/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/newproject/RecyclerViewAdapter.java
@@ -3,10 +3,13 @@ package com.example.newproject;
 import android.content.Context;
 import android.content.Intent;
 
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -20,17 +23,18 @@ import com.bumptech.glide.Glide;
 import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder> {
+    Integer id;
     private List<Datum> list;
     Context context;
     FragmentManager fragmentManager;
+    private static String bitmap="https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg";
 
 
-
-    public RecyclerViewAdapter(List<Datum> list, Context context , FragmentManager fragmentManager) {
+    public RecyclerViewAdapter(List<Datum> list, Context context, FragmentManager fragmentManager) {
 
         this.list = list;
         this.context = context;
-        this.fragmentManager=fragmentManager;
+        this.fragmentManager = fragmentManager;
 
     }
 
@@ -46,11 +50,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder> {
 
     public void onBindViewHolder(final ViewHolder holder, final int position) {
 
+        id = list.get(position).getId();
         holder.first_name.setText(list.get(position).getFirstName());
         holder.last_name.setText(list.get(position).getLastName());
         holder.email.setText(list.get(position).getEmail());
         Glide.with(context).load(list.get(position).getAvatar()).into(holder.avatar);
-
 
 
         holder.cv.setOnClickListener(new View.OnClickListener() {
@@ -66,8 +70,21 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder> {
                 transaction.replace(R.id.frame_layout,singleUserFragment);
                 transaction.commit();
 
-                singleUserFragment.setData();
 
+                Bundle bundle = new Bundle();
+                bundle.putString("Firstname", String.valueOf(list.get(position).getFirstName()));
+                bundle.putString("Lastname", String.valueOf(list.get(position).getLastName()));
+                bundle.putString("Email",String.valueOf(list.get(position).getEmail()));
+                bundle.putString("Avatar",String.valueOf(list.get(position).getAvatar()));
+
+                singleUserFragment.setArguments(bundle);
+                Log.d("LogTagId", String.valueOf(id));
+                Log.d("LogTagName", list.get(position).getFirstName());
+                Log.d("LogTagFamily", String.valueOf(list.get(position).getLastName()));
+                Log.d("LogtagEmail",String.valueOf(list.get(position).getEmail()));
+                Log.d("LogTag",(list.get(position).getAvatar()));
+
+                Log.d("LogTag","Clicled");
 
 
             }
@@ -77,7 +94,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder> {
 
 
     }
-
 
 
     @Override
--- app/src/main/java/com/example/newproject/SingleUserFragment.java
+++ app/src/main/java/com/example/newproject/SingleUserFragment.java
@@ -1,41 +1,66 @@
 package com.example.newproject;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
 
 public class SingleUserFragment extends Fragment {
+    private TextView Name;
+    private TextView Family;
+    private TextView Id;
+    private ImageView Image;
+    private TextView Email;
 
 
-
-
- public static SingleUserFragment newInstance() {
+    public static SingleUserFragment newInstance() {
         SingleUserFragment fragment = new SingleUserFragment();
         return fragment;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.pref_layout, container, false);
-        Toast.makeText(getContext(),"okey shod",Toast.LENGTH_LONG).show();
+        View view = inflater.inflate(R.layout.single_user, container, false);
+        Bundle bundle = getArguments();
 
+        String first_name = getArguments().getString("Firstname");
+        String last_name = getArguments().getString("Lastname");
+        String email = getArguments().getString("Email");
+        String avatar = getArguments().getString("Avatar");
 
+        Name = view.findViewById(R.id.name);
+        Family = view.findViewById(R.id.family);
+        Email = view.findViewById(R.id.email);
+        Image = view.findViewById(R.id.avatar);
+        Name.setText(first_name);
+        Family.setText(last_name);
+        Email.setText(email);
+        Glide.with(getContext()).load(avatar).into(Image);
+
+        Toast.makeText(getContext(), "Transaction Succesfull", Toast.LENGTH_LONG).show();
 
 
         return view;
 
     }
 
-    public void setData() {
 
-
-
+    public void onBackPressed() {
 
     }
+
+
 }
